4c3b187599224d7173e950a5aa6aacf304cd73b2
beyrerdn <beyrerdn@gmail.com>
2015-08-09 15:31:01 -0400

    Switched attributes back to User. Removed user_id (Profile) from the Devise form. Can't get Wizard to render.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,12 +4,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def create
+    binding.pry
     super
   end
 
   protected
 
   def after_sign_up_path_for(resource)
-    '/after_signup'
+    after_signup_path
   end
 end
